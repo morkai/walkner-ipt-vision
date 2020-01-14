f29847214673adf3a7ed02526b70c939d5743964
Łukasz Walukiewicz <lukasz@miracle.systems>
2020-01-14 14:09:39 +0100

    Manage firewall rules for the FTP server during install/uninstall.

--- ftp-server.install.bat
+++ ftp-server.install.bat
@@ -3,7 +3,32 @@
 set NSSM="%~dp0bin\nssm.exe"
 set NAME=walkner-ipt-vision-ftp-server
 
-%NSSM% install %NAME% "%~dp0bin\node.exe" """%~dp0lib\ftp-server.js"""
-%NSSM% set %NAME% AppNoConsole 1
-%NSSM% start %NAME%
+netsh advfirewall firewall show rule name=%name% > nul
+
+if errorlevel 1 (
+  echo Adding firewall rule %NAME%...
+  netsh advfirewall firewall add rule name=%NAME% dir=in protocol=TCP localport=21 program="%~dp0bin\node.exe" action=allow
+) else (
+  echo Firewall rule %NAME% already exists!
+)
+
+sc query %NAME% > nul
+
+if errorlevel 1 (
+  echo Installing service %NAME%...
+  %NSSM% install %NAME% "%~dp0bin\node.exe" """%~dp0lib\ftp-server.js"""
+  %NSSM% set %NAME% AppNoConsole 1
+) else (
+  echo Service %NAME% already exists!
+)
+
+sc query %NAME% | findstr "RUNNING" > nul
+
+if errorlevel 1 (
+  echo Starting the service...
+  %NSSM% start %NAME%
+) else (
+  echo Service already running!
+)
+
 pause
--- ftp-server.uninstall.bat
+++ ftp-server.uninstall.bat
@@ -3,6 +3,7 @@
 set NSSM="%~dp0bin\nssm.exe"
 set NAME=walkner-ipt-vision-ftp-server
 
+netsh advfirewall firewall delete rule name=%NAME%
 %NSSM% stop %NAME%
 %NSSM% remove %NAME% confirm
 pause
